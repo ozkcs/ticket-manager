02c6df043b9a121958ff1190675769be1107032d
ObandoAle <aobando@codinghubcr.net>
2022-09-20 16:41:31 -0600

    Feedback applied | Ident correction

--- src/services/eventsService.ts
+++ src/services/eventsService.ts
@@ -4,6 +4,6 @@ import { db } from "./firebaseService";
 const eventsCollectionRef = collection(db, "events");
 
 export const getEvents = async () => {
-	const data = await getDocs(eventsCollectionRef);
-	return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  const data = await getDocs(eventsCollectionRef);
+  return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 };
