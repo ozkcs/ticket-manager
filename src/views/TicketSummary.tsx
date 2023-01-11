150c9fae8711da154fce00af5f6850ea96d6e18f
joshmg-77 <joshmg276@gmail.com>
2023-01-11 10:31:50 -0600

    adds logic to send link of qrCode via WS

--- src/views/TicketSummary.tsx
+++ src/views/TicketSummary.tsx
@@ -11,7 +11,7 @@ import { MOCKED_SOLD_TICKETS } from '../data-mockups/sold_ticketMockup';
 
 const QRGenerator = () => {
   const eventsContext = useEvents();
-  const { orderID, pruchasedTickets, setPruchasedTickets } = eventsContext;
+  const { orderID, pruchasedTickets, setPruchasedTickets, currentOrder } = eventsContext;
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -32,6 +32,14 @@ const QRGenerator = () => {
     }
   }, []);
 
+  const buildWhatsAppLink = () => {
+    const numberValidator = '50687392610' || currentOrder?.phone 
+    const serviceURL = `https://wa.me/${numberValidator}?text=`
+    const message = `Estimado ${currentOrder?.first_name} en el siguiente link encontrarás tus tiquetes los cuáles pueden ser descargados\n `
+    const redirectUserTo = 'https://www.hermosasoftware.io/' || `https://localhost:3000/order/${orderID}`
+    return serviceURL.concat(message).concat(redirectUserTo)
+  }
+
   return (
     <>
       <EventBanner />
@@ -40,7 +48,7 @@ const QRGenerator = () => {
           <HStack justifyContent={'space-between'} width={'100%'}>
             <Heading mb={'10px'}>Select the tickets you want to send</Heading>
             <HStack justifyContent={'space-around'}>
-              <Button variant={'ghost'} colorScheme={'whatapp'}><IconBrandWhatsapp /> Send</Button>
+              <Button as="a" target="_blank" variant={'ghost'} colorScheme={'whatapp'} href={buildWhatsAppLink()}><IconBrandWhatsapp /> Send</Button>
               <Button variant={'ghost'} colorScheme={'messenger'}><IconMail /> Send</Button>
             </HStack>
           </HStack>
